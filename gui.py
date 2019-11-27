e8fa4b7424915f29a5335ec4f341b12779cba99f
Jonathan <theflametrooper@gmail.com>
2019-11-27 02:30:18 +0000

    refactor: map is now rendered using svg items
    Signed-off-by: Jonathan <theflametrooper@gmail.com>

--- gui.py
+++ gui.py
@@ -1,118 +1,141 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PySide2.QtWidgets import (QApplication, QGraphicsScene, QGraphicsView, QScrollArea, QAbstractSlider, QMainWindow, QWidget, QLabel, QDockWidget)
+from PySide2.QtWidgets import (QApplication, QGraphicsItem, QGraphicsScene, QGraphicsView, QScrollArea, QAbstractSlider, QMainWindow, QWidget, QLabel, QDockWidget)
 from PySide2.QtCore import *
 from PySide2.QtGui import *
+from PySide2.QtSvg import *
 
-countries = {
-    "224, 20, 20": "Canada",
-    "3, 50, 252": "United States",
-    "4, 91, 62": "Mexico"
-}
+class Country(QGraphicsSvgItem):
+	def __init__(self, parent, country):
+		super(Country, self).__init__()
 
-class InteractiveMap(QScrollArea):
-    def __init__(self, parent):
-        super(InteractiveMap, self).__init__(parent)
-        self.setWidgetResizable(False)
+		self.country = country
+		self.parent = parent
 
-        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+		self.setSharedRenderer(parent.renderer)
+		self.setElementId(self.country)
 
-        # prevents unintended scrolling with middle mouse button
-        self.horizontalScrollBar().setEnabled(False)
-        self.verticalScrollBar().setEnabled(False)
+	def mouseMoveEvent(self, event):
+		print("move")
 
-        # setup properties of class
-        self.isMouseMoving = False
+	def mouseReleaseEvent(self, event):
+		print("release")
 
-        self.displayMap(2500, 2500)
+	def mousePressEvent(self, event):
+		print(self.country)
+		self.parent.findCountry(self.country)
+		self.ungrabMouse()
 
-    def displayMap(self, width, height):
-        self.map = QImage("world_map.png").scaled(width, height, Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation)
-        
-        self.label = QLabel()
-        self.label.setScaledContents(True)
-        self.label.setPixmap(QPixmap.fromImage(self.map))
-        self.label.show()
+class InteractiveMap(QGraphicsView):
+	def __init__(self, parent):
+		super(InteractiveMap, self).__init__()
+		
+		self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+		self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
-        self.setWidget(self.label)
+		# prevents unintended scrolling with middle mouse button
+		self.horizontalScrollBar().setEnabled(False)
+		self.verticalScrollBar().setEnabled(False)
 
-    def scaleMap(self, zoom):
-        # zooming in and out of map
-        self.label.resize(
-            self.label.width() + zoom,
-            self.label.height() + zoom
-        )
+		self.displayMap()
+		self.setScene(self.scene)
+		self.scale(2, 2)
 
-    def wheelEvent(self, event):
-        movement = event.angleDelta().y()
-        
-        if (movement == 120):
-            self.scaleMap(500)
-        elif (movement == -120):
-            self.scaleMap(-500)
+		# setup properties of class
+		self.isMouseMoving = False
 
-    def findCountry(self, mousePosition):
-        color = self.map.pixelColor(mousePosition)
-        red = int(color.redF() * 255)
-        green = int(color.greenF() * 255)
-        blue = int(color.blueF() * 255)
-        rgb = "%d, %d, %d" % (red, green, blue)
+	def displayMap(self):
+		self.scene = QGraphicsScene()
 
-        # if colour matches to a country
-        country = QLabel()
-        if (rgb in countries):
-            country.setText(countries[rgb])
-        else:
-            country.setText("Not found")
-        country.setAlignment(Qt.AlignCenter)
+		self.renderer = QSvgRenderer("map.svg")
 
-        self.parent().showCountryDock(country)
-        print(countries[rgb])
+		self.ie = Country(self, "ie")
+		self.ie.setPos(1243.335, 223.772)
 
-    def mousePressEvent(self, event):
-        self.initialMousePosition = event.pos()
-        self.initialSliderPosition = {
-            "horizontal": self.horizontalScrollBar().value(),
-            "vertical": self.verticalScrollBar().value()
-        }
+		self.gb = Country(self, "gb")
+		self.gb.setPos(1260.61, 179.859)
 
-    def mouseMoveEvent(self, event):
-        self.isMouseMoving = True
+		self.es = Country(self, "es")
+		self.es.setPos(1169.683, 320.315)
 
-        changeX = self.initialMousePosition.x() - event.x()
-        changeY = self.initialMousePosition.y() - event.y()
+		self.fr = Country(self, "fr")
+		self.fr.setPos(834.585, 258.535)
 
-        self.horizontalScrollBar().setValue(self.initialSliderPosition["horizontal"] + changeX)
-        self.verticalScrollBar().setValue(self.initialSliderPosition["vertical"] + changeY)
+		self.ru = Country(self, "ru")
+		self.ru.setPos(1439.66, 32.788)
 
-    def mouseReleaseEvent(self, event):
-        if (not(self.isMouseMoving)):
-            self.findCountry(event.pos() + QPoint(self.horizontalScrollBar().value(), self.verticalScrollBar().value()))
-        self.isMouseMoving = False
-    
+		self.scene.addItem(self.gb)
+		self.scene.addItem(self.ie)
+		self.scene.addItem(self.es)
+		self.scene.addItem(self.fr)
+		self.scene.addItem(self.ru)
+
+	def mousePressEvent(self, event):
+		self.initialMousePosition = event.pos()
+		self.initialSliderPosition = {
+			"horizontal": self.horizontalScrollBar().value(),
+			"vertical": self.verticalScrollBar().value()
+		}
+
+	def mouseMoveEvent(self, event):
+		self.isMouseMoving = True
+
+		changeX = self.initialMousePosition.x() - event.x()
+		changeY = self.initialMousePosition.y() - event.y()
+
+		self.horizontalScrollBar().setValue(self.initialSliderPosition["horizontal"] + changeX)
+		self.verticalScrollBar().setValue(self.initialSliderPosition["vertical"] + changeY)
+
+	def mouseReleaseEvent(self, event):
+		self.isMouseMoving = False
+
+	def scaleMap(self, zoom):
+		# zooming in and out of map
+		self.label.resize(
+			self.label.width() + zoom,
+			self.label.height() + zoom
+		)
+
+	def wheelEvent(self, event):
+		movement = event.angleDelta().y()
+
+		if (movement == 120):
+			self.scaleMap(500)
+		elif (movement == -120):
+			self.scaleMap(-500)
+
+	def findCountry(self, country):
+		text = QLabel()
+		if (True):
+			text.setText(country)
+		else:
+			text.setText("Not found")
+			text.setAlignment(Qt.AlignCenter)
+
+		self.parent().showCountryDock(text)
+
 class Window(QMainWindow):
-    def __init__(self):
-        super(Window, self).__init__()
+	def __init__(self):
+		super(Window, self).__init__()
 
-        interactiveMap = InteractiveMap(self)
-        self.setCentralWidget(interactiveMap)
-        
-        self.countryInfo = QDockWidget("Country Information", self)
-        self.countryInfo.setFeatures(QDockWidget.DockWidgetClosable)
-        self.countryInfo.setFixedWidth(300)
-        self.countryInfo.hide()
+		interactiveMap = InteractiveMap(self)
+		self.setCentralWidget(interactiveMap)
+		
+		self.countryInfo = QDockWidget("Country Information", self)
+		self.countryInfo.setFeatures(QDockWidget.DockWidgetClosable)
+		self.countryInfo.setFixedWidth(300)
+		self.countryInfo.hide()
 
-        self.addDockWidget(Qt.RightDockWidgetArea, self.countryInfo)
+		self.addDockWidget(Qt.RightDockWidgetArea, self.countryInfo)
 
-        self.setWindowTitle("Project Fiscal")
-        self.showMaximized()
+		self.setWindowTitle("Project Fiscal")
+		self.showMaximized()
 
-    def showCountryDock(self, country):
-        if (self.countryInfo.isHidden()):
-            self.countryInfo.show()
-        self.countryInfo.setWidget(country)
+	def showCountryDock(self, country):
+		if (self.countryInfo.isHidden()):
+			self.countryInfo.show()
+		self.countryInfo.setWidget(country)
 
 app = QApplication(sys.argv)
 gui = Window()
